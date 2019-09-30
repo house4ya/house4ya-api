355523c4801b625f2eca99ff047fb526c5d80743
jesus1989garcia <jesus1989garcia@gmail.com>
2019-09-30 18:39:08 +0200

    added login and passport config

--- house4ya-api/controllers/authController.js
+++ house4ya-api/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/users-model')
 const createError = require('http-errors')
+const passport = require('passport')
 
 module.exports.register = (req, res, next) => {
   User.findOne( { email: req.body.email })
@@ -9,4 +10,22 @@ module.exports.register = (req, res, next) => {
     })
     .then(user => res.status(201).json(user))
     .catch(next)
+}
+
+module.exports.login = (req, res, next) => {
+  passport.authenticate('auth-local', (error, user, message) => {
+    if(error) {
+      next(error)
+    }else if(!user) {
+      next(createError(401, message))
+    }else {
+      req.login(user, (error) => {
+        if (error) {
+          next(error)
+        }else {
+          res.status(201).json(user)
+        }
+      })
+    }
+  })(req, res, next)
 }
