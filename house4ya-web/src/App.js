8e36d733e7b45c0e67f0aa17b6952867b3ee2b05
jesus1989garcia <jesus1989garcia@gmail.com>
2020-03-21 20:37:20 +0100

    feat/basic login added

--- house4ya-web/src/App.js
+++ house4ya-web/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import NavBar from './misc/NavBar';
 import Card from './components/Cards';
+import Login from './components/login';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <div>
           <NavBar />
           <Card />
+          <Login />
         </div>
         
     </div>
