76c4f075a1046d3e3f43ffcca97459a8cdf0a8a4
jesus1989garcia <jesus1989garcia@gmail.com>
2020-03-22 12:33:35 +0100

    added basic routing

--- house4ya-web/src/App.js
+++ house4ya-web/src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './App.css';
 import NavBar from './misc/NavBar';
-import Card from './components/Cards';
+// import Card from './components/Cards';
 import Login from './components/login';
 import Register from './components/Register';
+import { Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-        <div>
-          <NavBar />
-          <Card />
-          <Login />
-          <Register />
-        </div>
+     <NavBar/>
+     <Switch>
+       <Route path='/home'  />
+      <Route path='/signin' component={ Login } />
+      <Route  path='/signup' component={ Register } />
+     </Switch>
+      
+       
         
     </div>
   );
