6cf1071f36d99a390bbae028ae0e31caac572bb6
jesus1989garcia <jesus1989garcia@gmail.com>
2020-04-06 14:35:15 +0200

    single house card added

--- house4ya-web/src/components/Cards.js
+++ house4ya-web/src/components/Cards.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Card extends Component {
   constructor(props){
@@ -7,7 +8,7 @@ class Card extends Component {
 
   render() {
     return(
-      <div class="card">
+      <Link class="card" to={this.props.house.id}>
   <div class="card-image">
     <figure class="image is-4by3">
       <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
@@ -27,14 +28,13 @@ class Card extends Component {
     </div>
 
     <div class="content">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+      <p>{this.props.house.description}</p> <a>@bulmaio</a>.
       <a href="#">#css</a> <a href="#">#responsive</a>
       <br/>
       <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
     </div>
   </div>
-</div>
+</Link>
     )
   }
 }
